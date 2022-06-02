023f9becfb4784524419686685a784c46cd1b093
Fasola Olabode <77270775+Bodmarn@users.noreply.github.com>
2022-06-02 22:09:58 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -3,24 +3,24 @@
 # count_words("The cake is done. It is a big cake!") 
 # --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
 
+import string
+
+
 def read_file_content(filename):
     # [assignment] Add your code here 
-    with open("story.txt", "r") as file:
-        var = file.read()
-        print(var)
-        file.close() 
-    
-    return "Hello World"
+    with open(filename) as file:
+        file_content = file.read()
+    return file_content
 
 
 def count_words():
     text = read_file_content("story.txt")
     # [assignment] Add your code here
-    count_words("story.txt")
-    for x in ():
-        count_words += 1
+    text = text.translate(str.maketrans("","", string.punctuation))
+    word_count = {}
+    split_text = text.split()
+
+    for word in split_text:
+        word_count[word] = split_text.count(word)
+    return word_count
         
-
-    
-
-    return {"as": 10, "would": 20}
